71225300cb5227a104e0cc1f45360c12727bef61
vincenzo <speedvicio@hotmail.com>
2019-01-31 14:17:15 +0100

    Aggiungere i file di progetto.

---
+++ WindowsApplication1/MedInstaller.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MedInstaller
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MedInstaller))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 134)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "&Yes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(120, 134)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "&No"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(17, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(187, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Do you want install MedGui Reborn?"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(197, 85)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Overwrite existing?"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Enabled = False
+        Me.CheckBox2.Location = New System.Drawing.Point(8, 51)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(119, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Net-Play Server List"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Enabled = False
+        Me.CheckBox1.Location = New System.Drawing.Point(8, 28)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(110, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "UCI Channels List"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(18, 167)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(184, 17)
+        Me.CheckBox3.TabIndex = 4
+        Me.CheckBox3.Text = "Open MedGui Reborn Changelog"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(221, 196)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MedGui Reborn Installer"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+End Class
---
+++ WindowsApplication1/MedInstaller.vb
@@ -0,0 +1,89 @@
+﻿Imports System.IO
+
+Public Class MedInstaller
+    Dim cartella As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If File.Exists(Path.Combine(cartella, "MedGuiR\Update\MedGuiR.exe")) = False Then
+            MsgBox("No MedGuiR update detected, I will close the installer", vbOKOnly + MsgBoxStyle.Exclamation, "Update not detected...")
+            Me.Close()
+            Exit Sub
+        End If
+
+        Dim processesByName As Process() = Process.GetProcessesByName("MedGuiR")
+        Dim num As Integer = 0
+        While num < CInt(processesByName.Length)
+            processesByName(num).Kill()
+            num = num + 1
+        End While
+
+        If File.Exists(Path.Combine(cartella, "MedGuiR\ListServer.txt")) And CheckBox2.Checked = False Then File.Delete(cartella & "MedGuiR\Update\MedGuiR\ListServer.txt")
+        If File.Exists(Path.Combine(cartella, "MedGuiR\UCI.txt")) And CheckBox1.Checked = False Then File.Delete(cartella & "MedGuiR\Update\MedGuiR\UCI.txt")
+
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cartella = Application.StartupPath
+
+        If File.Exists(Path.Combine(cartella, "MedGuiR\ListServer.txt")) Then CheckBox2.Enabled = True
+        If File.Exists(Path.Combine(cartella, "MedGuiR\UCI.txt")) Then CheckBox1.Enabled = True
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Public Sub elimino_temp()
+        'Elimino i file al termine dell'update
+        Try
+            Directory.Delete(Path.Combine(cartella, "MedGuiR\Update\"), True)
+        Catch
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Enabled = False
+        Dim attemp As Integer = 0
+        Try
+            Dim infoReader As FileInfo
+            infoReader = My.Computer.FileSystem.GetFileInfo(Path.Combine(cartella, "MedGuiR\Update\MedGuiR.exe"))
+
+            If Dir(Path.Combine(cartella, "MedGuiR\Update\MedGuiR.exe")) = "" Or infoReader.Length < 100 Then
+                MsgBox("No MedGui Reborn update found, please reupdate MedGui Reborn and try again.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Update error")
+            Else
+                Button1.Enabled = False
+                Label1.Text = "Wait...."
+
+                File.Delete(Path.Combine(cartella, "MedGuiR\Update\update.txt"))
+URETRY:
+                My.Computer.FileSystem.MoveDirectory(Path.Combine(cartella, "MedGuiR\Update\"), cartella & "\", True)
+                MsgBox("Update Done, i will run MedGui Reborn.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Update Done")
+            End If
+
+            elimino_temp()
+            Process.Start(Path.Combine(cartella, "MedGuiR.exe"))
+            If File.Exists(Path.Combine(cartella, "Changelog MedGuiR.txt")) And CheckBox3.Checked = True Then Process.Start(Path.Combine(cartella, "Changelog MedGuiR.txt"))
+            Me.Close()
+        Catch
+
+            attemp += 1
+
+            If attemp < 6 Then
+                GoTo URETRY
+            Else
+                MsgBox("Any file seem locked by OS, try to move it manually on main MedGui Reborn folder after closing MedInstaller", vbCritical + vbOKOnly, "Unable to move file...")
+                Process.Start(Path.Combine(cartella, "MedGuiR\Update\"))
+                If File.Exists(Path.Combine(cartella, "MedGuiR.exe")) Then
+                    Process.Start(Path.Combine(cartella, "MedGuiR.exe"))
+                Else
+                    MsgBox("Unable to recognize MedGui Reborn", vbCritical + vbOKOnly)
+                End If
+                Me.Close()
+            End If
+        End Try
+    End Sub
+
+End Class
